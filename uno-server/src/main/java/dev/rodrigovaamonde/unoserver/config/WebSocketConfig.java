3037921a6f6326876a5952a65c208809110b3d17
Rodrigo Vaamonde <rvaamont@mercadona.es>
2025-07-31 12:24:22 +0200

    add WebSocket configuration for message broker and STOMP endpoints

---
+++ uno-server/src/main/java/dev/rodrigovaamonde/unoserver/config/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package dev.rodrigovaamonde.unoserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(org.springframework.web.socket.config.annotation.StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+    }
+}
