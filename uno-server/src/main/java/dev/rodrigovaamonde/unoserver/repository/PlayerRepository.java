183034977b7c41ccc804bfa98c32ebc9362863d4
Rodrigo Vaamonde <vaamonde.rodri@gmail.com>
2025-07-31 02:20:16 +0200

    add repositories for Game and Player to manage persistence and retrieval

---
+++ uno-server/src/main/java/dev/rodrigovaamonde/unoserver/repository/PlayerRepository.java
@@ -0,0 +1,12 @@
+package dev.rodrigovaamonde.unoserver.repository;
+
+import dev.rodrigovaamonde.unoserver.model.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+    Optional<Player> findByName(String name);
+}
